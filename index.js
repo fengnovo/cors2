79fe95cd12d1d0c9ea0ec1bb720252ee57f6b964
fengnovo <fengnoku@gmail.com>
2022-09-26 11:33:15 +0800

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require("express");
+const cors = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// 使用代理
+var app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3001"], // 所要允许跨域的ip
+    methods: ["GET", "POST"],
+    alloweHeaders: ["Conten-Type", "Authorization"],
+  })
+);
+
+// app.use(
+//   createProxyMiddleware({
+//     target: "https://news-at.zhihu.com/",
+//     changeOrigin: true,
+//     pathRewrite: {
+//       "^/": "/",
+//     },
+//   })
+// );
+
+app.get("*", function routeHandler(req, res) {
+  const { target } = req.query || {};
+  createProxyMiddleware({
+    target: decodeURIComponent(target),
+    changeOrigin: true,
+    pathRewrite: {
+      "^/": "/",
+    },
+  })(req, res);
+});
+
+app.listen(3000);
